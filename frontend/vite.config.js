e9e6f460ec3137b5d1b612f2e537463fe46e7d8e
m4lachi <mz376@njit.edu>
2025-10-26 02:04:05 -0400

    minor updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+       '/auth': 'http://localhost:3000',
+      '/applications': 'http://localhost:3000'
     }
   }
 })
